e7272b0c7d013fe926a69086a4fa085d344b45d3
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-05-02 14:34:23 +0100

    sorting linters errors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let books = JSON.parse(localStorage.getItem('books')) || []
+let books = JSON.parse(localStorage.getItem('books')) || [];
 
 const bookList = document.getElementById('book-list');
 const authorInput = document.getElementById('author');
@@ -6,51 +6,51 @@ const titleInput = document.getElementById('title');
 const addBtn = document.getElementById('add');
 
 function addBook(author, title) {
-    const content = {author, title}
+  const content = { author, title };
 
-    books.push(content)
+  books.push(content);
 
-    localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 
-    return content
+  return content;
 }
 
 // to remove book
 
 function removeBook(title) {
-    books = books.filter(book => book.title !== title)
-    localStorage.setItem('books', JSON.stringify(books))
+  books = books.filter((book) => book.title !== title);
+  localStorage.setItem('books', JSON.stringify(books));
 }
 
-// to display book to collection 
+// to display book to collection
 
 function displayBooks() {
-    bookList.innerHTML = '';
-    books.forEach(book => {
-      const li = document.createElement('li');
-      li.textContent = `${book.title}`;
-      li.textContent = ` ${book.author}`;
-      const removeBtn = document.createElement('button');
-      removeBtn.textContent = 'Remove';
-      removeBtn.addEventListener('click', () => {
-        removeBook(book.title);
-        displayBooks();
-      });
-      li.appendChild(removeBtn);
-      bookList.appendChild(li);
+  bookList.innerHTML = '';
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    li.textContent = `${book.title}`;
+    li.textContent = ` ${book.author}`;
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {
+      removeBook(book.title);
+      displayBooks();
     });
+    li.appendChild(removeBtn);
+    bookList.appendChild(li);
+  });
 }
 
- // Add event listener to the Add button
+// Add event listener to the Add button
 addBtn.addEventListener('click', () => {
-    const title = titleInput.value.trim();
-    const author = authorInput.value.trim();
-    if (title && author) {
-      addBook(title, author);
-      displayBooks();
-      titleInput.value = '';
-      authorInput.value = '';
-    }
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
+  if (title && author) {
+    addBook(title, author);
+    displayBooks();
+    titleInput.value = '';
+    authorInput.value = '';
+  }
 });
 
 // Display the initial collection of books
