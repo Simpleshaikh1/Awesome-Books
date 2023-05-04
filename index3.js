dee20624c76435f93ee612613a1768edb6253676
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-05-04 13:33:57 +0100

    link sorted

--- index3.js
+++ index3.js
@@ -50,5 +50,67 @@ class BookList {
     this.display();
   }
 }
+
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const btn = document.querySelector('#add');
+
 const lists = new BookList();
-lists();
+lists.display();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+
+  lists.addBook(newBook);
+  form.reset();
+});
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const newBook = {
+    title: title.value,
+    author: title.value,
+  };
+
+  lists.addBook(newBook);
+  form.reset();
+});
+
+const listNav = document.querySelector('#list');
+const addNav = document.querySelector('#add-new');
+const contactNav = document.querySelector('#contact');
+
+const contact = document.querySelector('.contact-info-section');
+const books = document.querySelector('.list-section');
+const addBook = document.querySelector('.form1');
+// const awesome = document.querySelector('#awesome-heading');
+
+
+const showBooks = () => {
+  contact.style.display = 'none';
+  books.style.display = 'block';
+  addBook.style.display = 'none';
+  // awesome.style.display = '';
+};
+
+const showAdd = () => {
+  contact.style.display = 'none';
+  books.style.display = 'none';
+  addBook.style.display = 'block';
+  // awesome.style.display = 'none';
+};
+
+const showContact = () => {
+  contact.style.display = 'block';
+  books.style.display = 'none';
+  addBook.style.display = 'none';
+  // awesome.style.display = 'none';
+};
+listNav.addEventListener('click', showBooks);
+addNav.addEventListener('click', showAdd);
+contactNav.addEventListener('click', showContact);
