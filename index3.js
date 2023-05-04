ece0b053d3eef99799de0ce6e1d6fffb1357fde6
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-05-04 14:12:26 +0100

    completed

--- index3.js
+++ index3.js
@@ -88,28 +88,27 @@ const contactNav = document.querySelector('#contact');
 const contact = document.querySelector('.contact-info-section');
 const books = document.querySelector('.list-section');
 const addBook = document.querySelector('.form1');
-// const awesome = document.querySelector('#awesome-heading');
-
+const awesome = document.querySelector('#awesome-heading');
 
 const showBooks = () => {
   contact.style.display = 'none';
   books.style.display = 'block';
   addBook.style.display = 'none';
-  // awesome.style.display = '';
+  awesome.style.display = '';
 };
 
 const showAdd = () => {
   contact.style.display = 'none';
   books.style.display = 'none';
   addBook.style.display = 'block';
-  // awesome.style.display = 'none';
+  awesome.style.display = 'none';
 };
 
 const showContact = () => {
   contact.style.display = 'block';
   books.style.display = 'none';
   addBook.style.display = 'none';
-  // awesome.style.display = 'none';
+  awesome.style.display = 'none';
 };
 listNav.addEventListener('click', showBooks);
 addNav.addEventListener('click', showAdd);
