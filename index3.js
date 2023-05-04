71bb4f1332729f8af50c9e070a894c3f58c7dbda
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-05-04 16:25:13 +0100

    Local-storage sorted

--- index3.js
+++ index3.js
@@ -64,6 +64,7 @@ const addBook = document.querySelector('.form1');
 const awesome = document.querySelector('#awesome-heading');
 
 const showBooks = () => {
+  localStorage.setItem('currentSection', 'list');
   contact.style.display = 'none';
   books.style.display = 'block';
   addBook.style.display = 'none';
@@ -71,13 +72,16 @@ const showBooks = () => {
 };
 
 const showAdd = () => {
+  localStorage.setItem('currentSection', 'add');
   contact.style.display = 'none';
   books.style.display = 'none';
   addBook.style.display = 'block';
   awesome.style.display = 'none';
+  showAdd();
 };
 
 const showContact = () => {
+  localStorage.setItem('currentSection', 'contact');
   contact.style.display = 'block';
   books.style.display = 'none';
   addBook.style.display = 'none';
@@ -87,3 +91,12 @@ const showContact = () => {
 listNav.addEventListener('click', showBooks);
 addNav.addEventListener('click', showAdd);
 contactNav.addEventListener('click', showContact);
+
+const currentSection = localStorage.getItem('currentSection');
+if (currentSection === 'add') {
+  showAdd();
+} else if (currentSection === 'contact') {
+  showContact();
+} else {
+  showBooks();
+}
